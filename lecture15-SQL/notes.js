00d9370a16c9d3f9c9aec0bedf5e756394178d06
Aryan Dhiman <aryandhiman2605@gmail.com>
2026-01-17 23:58:32 +0530

    update new folder

---
+++ lecture15-SQL/notes.js
@@ -0,0 +1,3 @@
+//npm init -y
+//npm install tailwindcss @tailwindcss/cli postcss autoprefixer
+//npx @tailwindcss/cli -i ./views/input.css -o ./public/output.css --watch
---
+++ lecture15-SQL/routes/hostRouter.js
@@ -0,0 +1,21 @@
+//External module
+const express = require('express');
+const hostRouter = express.Router();
+
+//Local Modules
+const hostController  = require("../controllers/hostController");
+
+hostRouter.get("/add-home", hostController.getAddHome);  //Route of get AddHome request
+
+hostRouter.post("/add-home", hostController.postAddHome);  //Route of post AddHome request
+
+hostRouter.get("/host-home-list", hostController.getHostHomes);
+
+hostRouter.get("/edit-home/:homeId", hostController.getEditHome);
+hostRouter.post("/edit-home", hostController.postEditHome);
+
+hostRouter.post("/delete-home/:homeId", hostController.postDeleteHome);
+
+
+
+module.exports = hostRouter;

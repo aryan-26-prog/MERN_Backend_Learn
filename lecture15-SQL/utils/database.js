5e0b571cffac20b141268d720b2804ae030f52db
Aryan Dhiman <aryandhiman2605@gmail.com>
2026-01-22 01:46:44 +0530

    Update database.js

--- lecture15-SQL/utils/database.js
+++ lecture15-SQL/utils/database.js
@@ -4,8 +4,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({  //It is used to manage and establish a connection pool
   host: "localhost",
   user: "root",
-  password: "Aryan#26@",
-  database: "airbnb"
+  password: "****",
+  database: "****"
 }); 
 
 module.exports = pool.promise();  //This allows you to interact with the MySQL database using the modern JavaScript async/await syntax 
